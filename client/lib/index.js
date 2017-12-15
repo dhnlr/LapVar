25d490663f41e4e1c7d95f738914aa4e03057163
andrey <andrey.amrulloh@gmail.com>
2017-12-15 10:11:45 +0700

    Benerin Bug

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,4 +1,3 @@
-console.log(navigator.geolocation)
 
 function initialize(selector) {
   if (navigator.geolocation) {
@@ -9,30 +8,30 @@ function initialize(selector) {
     var lng = position.coords.longitude;
     $("#long").html(lat);
     $("#lang").html(lng);
-  }
+    $.ajax({
+    type: 'GET',
+    url: `http://localhost:3000/zomato?lat=${lat}&lng=${lng}`,
+    success: function (response) {
+      console.log(response)
+      $("#long").empty()
+      response.forEach(showData =>{
+        $("#long").append(
+          `<div class="item">
+          <img class="ui avatar image" src="">
+          <div class="content">
+              <a class="header">${showData.restaurant.name}</a>
+              <div class="description">${showData.restaurant.cuisines}
+          </div>
+        </div>`
 
+        )
+      })
+    }
+  })
+  }
   function errorFunction() {
     alert("Geocoder failed");
   }
 
-  // $.ajax({
-  //   type: 'GET',
-  //   url: `http://localhost:3000/zomato?lat=-6.2601516&lng=106.781753099`,
-  //   success: function (response) {
-  //     console.log(response)
-  //     $(selector).empty()
-  //     response.forEach(showData =>{
-  //       $(selector).append(
-  //         `<div class="item">
-  //         <img class="ui avatar image" src="">
-  //         <div class="content">
-  //             <a class="header">${showData.restaurant.name}</a>
-  //             <div class="description">${showData.restaurant.cuisines}
-  //         </div>
-  //       </div>`
-
-  //       )
-  //     })
-  //   }
-  // })
+  
 }
